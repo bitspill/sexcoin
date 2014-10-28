70372413f6722f70b5f4fb2205f7c6d79b6741aa
Jeremiah Buddenhagen <bitspill+github@bitspill.net>
2014-10-28 06:07:14 -0500

    Add OP_CHECKSTATE

--- src/script.h
+++ src/script.h
@@ -189,7 +189,8 @@ enum opcodetype
     OP_NOP9 = 0xb8,
     OP_NOP10 = 0xb9,
 
-
+    // Automatic Transaction
+    OP_CHECKSTATE = 0xf6,
 
     // template matching params
     OP_SMALLINTEGER = 0xfa,
